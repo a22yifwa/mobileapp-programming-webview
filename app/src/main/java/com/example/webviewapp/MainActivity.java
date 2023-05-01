aa8a11e3c18b8b1abc64a81bc5bab594575ae1ce
a22yifwa <a22yifwa@student.his.se>
2023-05-01 17:45:43 +0200

    Skapa URL och call howExternalWebPage()` `showInternalWebPage()`

--- app/src/main/java/com/example/webviewapp/MainActivity.java
+++ app/src/main/java/com/example/webviewapp/MainActivity.java
@@ -14,13 +14,10 @@ import androidx.appcompat.widget.Toolbar;
 
 public class MainActivity extends AppCompatActivity {
    private WebView myWebView;
-    public void showExternalWebPage(){myWebView.loadUrl("https://his.se");
-        // TODO: Add your code for showing external web page here
-    }
+    public void showExternalWebPage() {myWebView.loadUrl("https://his.se"); }
 
-    public void showInternalWebPage(){
-        // TODO: Add your code for showing internal web page here
-    }
+
+    public void showInternalWebPage(){} {myWebView.loadUrl("file:///android_asset/webb.html"); }
 
 
     @Override
@@ -75,11 +72,13 @@ public class MainActivity extends AppCompatActivity {
 
         //noinspection SimplifiableIfStatement
         if (id == R.id.action_external_web) {
+            showExternalWebPage();
             Log.d("==>","Will display external web page");
             return true;
         }
 
         if (id == R.id.action_internal_web) {
+            showInternalWebPage();
             Log.d("==>","Will display internal web page");
             return true;
         }
